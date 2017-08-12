3b2cffdd14827c2e9195ba1da041b22c29ba877d
Don McGregor <mcgredo@nps.edu>
2017-08-12 08:56:21 -0700

    Some documentation for pandoc

--- tutorial/runPandoc.sh
+++ tutorial/runPandoc.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
+# Reads a file that has an in-order list of .md files. 
+# this list is provided to pandoc, which in effect
+# concatenates them, and then uses the concatenated
+# file to generate other formats. This includes epub,
+# which is used in most ebooks, html, and pdf. 
+#
+# pandoc is at https://pandoc.org/
+# Generated pdf requires LaTeX, available at https://www.latex-project.org/get/
+# Example pandoc efforts: http://pandoc.org/demos.html
+
 mdFilesArray=( `cat "pandoc_MD_order.txt" `)
 echo ${mdFilesArray[@]}
 
-#pandoc -S -o tutorial.epub I_Introduction/DIS_Background.md I_Introduction/DIS_History.md I_Introduction/DIS_Examples.md
-
 pandoc -S -o tutorial.epub ${mdFilesArray[@]} 
+pandoc -S -o tutorial.html ${mdFilesArray[@]}
